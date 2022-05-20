1bfcb1e7463e50d1494ce8cf4a926fa854bb0417
xxx <xxx@xxx.com>
2022-05-20 08:42:30 +0800

    hive window t2 t3

--- src/main/scala/com/hive/HiveWindowFunTest2.scala
+++ src/main/scala/com/hive/HiveWindowFunTest2.scala
@@ -24,6 +24,7 @@ object HiveWindowFunTest2 {
 
 
     //todo  模拟hive 测试二
+    //     主要是分析函数：聚合类 + 偏移量类lag lead ntile
     //1、查询在2017年4月份购买过的顾客及总人数
     //    hiveQuery("select *,count(name)over()as total from business where substr(orderdate,1,7)='2017-04'")
 
@@ -38,13 +39,7 @@ object HiveWindowFunTest2 {
 
     // 5、查询前20%时间的订单信息
     //ntile(n) 把有序分区中的行分发到指定数据的组中，各个组有编号，编号从1开始，对于每一行，ntile返回此行所属的组的编号
-    hiveQuery("select *,from ( ) t where t.sortgroup_num=1")
-
-//    select  *
-    //from
-    //(select   *,
-    //ntile(5)over(order  by  cost)sortgroup_num from  business)t
-    //where t.sortgroup_num = 1;
+    hiveQuery("select * from (select *,ntile(5)over(order by orderdate) as sortgroup_num from business) t where t.sortgroup_num=1")
 
 
     def hiveQuery(sql: String): Unit = {
