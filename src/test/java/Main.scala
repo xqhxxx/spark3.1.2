03023fe4f6a7f1657e130a6dda0e26e98b5326ab
xxx <xxx@xxx.com>
2022-09-07 14:25:49 +0800

    0906

--- src/test/java/Main.scala
+++ src/test/java/Main.scala
@@ -19,20 +19,17 @@ object Main {
     val df = ss.read
       .option("header", value = true)
       .csv("datas/hist.csv")
-
-
     df.createOrReplaceTempView("hist_tb")
-//    df.show(false)
-
     val df2 = ss.read
       .option("header", value = true)
       .csv("datas/tb.csv")
-
     df2.createOrReplaceTempView("kks_tb")
-//    df2.show(false)
 
     ss.sql(
-      s"select nvl(),av from hist_tb a left join kks_tb b where a.kks=b.kks1 ")
+//      s"select a.kks,nvl(b.kks2,a.kks) as kks_update,av,b.kks1,b.kks2 from hist_tb a left join kks_tb b on a.kks=b.kks1 "
+
+    s"select a.kks, (case when a.kks='k01' then 'k02'  when a.kks='k02' then 'k01' else a.kks end ) as kks_update,av from hist_tb a  "
+    )
 
       .show(false)
 
